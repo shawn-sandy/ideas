b6df13ff14f3ea25f591ad6de7c1592992614557
Shawn Sandy <38263979+shawn-sandy@users.noreply.github.com>
2020-04-04 16:42:28 -0400

    404 page eleventy config

--- .eleventy.js
+++ .eleventy.js
@@ -1,7 +1,23 @@
-module.exports = function(eleventyConfig) {
+const fs = require("fs")
+
+
+module.exports = function (eleventyConfig) {
   eleventyConfig.setBrowserSyncConfig({
     notify: true,
-    open: true
+    open: true,
+    callbacks: {
+      ready: function(err, bs) {
+        bs.addMiddleware("*", (req, res) => {
+          const content_404 = fs.readFileSync("www/404.html");
+          // Provides the 404 content without redirect.
+          res.write(content_404);
+          // Add 404 http status code in request header.
+          // res.writeHead(404, { "Content-Type": "text/html" });
+          res.writeHead(404);
+          res.end();
+        });
+      }
+    }
   });
   eleventyConfig.addPassthroughCopy("**/dist/styles/css");
   eleventyConfig.addPassthroughCopy("**/dist/js");
