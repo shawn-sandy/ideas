19eaf809aeb8442be79e013ad3add42fd022316c
Shawn Sandy <38263979+shawn-sandy@users.noreply.github.com>
2020-04-08 08:46:29 -0400

    System debug feature--first commit

--- system/debug/.eleventy.js
+++ system/debug/.eleventy.js
@@ -6,27 +6,7 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.setBrowserSyncConfig({
     notify: true,
     open: true,
-    callbacks: {
-      ready: function (err, bs) {
-        bs.addMiddleware("*", (req, res) => {
-          const content_404 = fs.readFileSync("www/404.html");
-          // Provides the 404 content without redirect.
-          res.write(content_404);
-          // Add 404 http status code in request header.
-          // res.writeHead(404, { "Content-Type": "text/html" });
-          res.writeHead(404);
-          res.end();
-        });
-      },
-    },
   });
-  eleventyConfig.addPassthroughCopy("**/dist/styles/css");
-  eleventyConfig.addPassthroughCopy("**/dist/js");
-  eleventyConfig.addPassthroughCopy("**/dist/images");
-
-  eleventyConfig.addPassthroughCopy("system/styles/css", "css");
-  eleventyConfig.addPassthroughCopy("system/js/*.js", "js");
-  eleventyConfig.addPassthroughCopy("system/images", "images");
 
   eleventyConfig.addPlugin(require("@shawnsandy/ideas"));
 
@@ -45,10 +25,10 @@ module.exports = function (eleventyConfig) {
 
   return {
     dir: {
-      output: "./www",
+      output: "../../www/debug",
       input: ".",
-      includes: "/system/_includes",
-      data: "/system/_data",
+      includes: "../_includes",
+      data: "../_data",
     },
     templateFormats: ["njk", "html"],
     htmlTemplateEngine: "njk",
