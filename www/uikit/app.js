b5533ef2be2fb781d471552451045c3c0ccadd4b
Shawn Sandy <38263979+shawn-sandy@users.noreply.github.com>
2020-04-12 19:09:08 -0400

    build/bundle update

--- www/uikit/app.js
+++ www/uikit/app.js
@@ -256,7 +256,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "51776" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "53002" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
