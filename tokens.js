3200d9c54b874523119427b3fa0e6984751c9102
Shawn Sandy <38263979+shawn-sandy@users.noreply.github.com>
2020-04-13 07:55:15 -0400

    Moves system/token folder into _data

--- tokens.js
+++ tokens.js
@@ -2,7 +2,7 @@
 const config = require("@shawnsandy/mix/config/tokens.js");
 
 module.exports = config({
-  source: ["system/tokens/**/*.json"], // tokens dir--/tokens/my-tokens.json
+  source: ["system/_data/tokens/**/*.json"], // tokens dir--/tokens/my-tokens.json
   build: "styles/_tokens/", // build dir trailing slash required--scss/_tokens.scss
   name: "tokens", // name of you scss token output file--tokens.scss
 });
