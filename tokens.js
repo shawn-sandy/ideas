cd54aafbf9580f0a40e999a8e169ca44e0a84f1e
Shawn Sandy <38263979+shawn-sandy@users.noreply.github.com>
2020-04-17 07:09:08 -0400

    resolves issue -- no color on maps otuput

--- tokens.js
+++ tokens.js
@@ -1,8 +1,9 @@
 // config.js
 const config = require("@shawnsandy/mix/config/tokens.js");
+// const config = require("./system/styles/_tokens/_tokens.scss");
 
 module.exports = config({
   source: ["system/_data/tokens/**/*.json"], // tokens dir--/tokens/my-tokens.json
-  build: "styles/_tokens/", // build dir trailing slash required--scss/_tokens.scss
+  build: "system/styles/_tokens/", // build dir trailing slash required--scss/_tokens.scss
   name: "_tokens", // name of you scss token output file--tokens.scss
 });
