1812e88761b22232f5bfe43c432cf4c3b5609313
Shawn Sandy <38263979+shawn-sandy@users.noreply.github.com>
2020-04-16 23:07:44 -0400

    WIP

--- tokens.js
+++ tokens.js
@@ -4,5 +4,5 @@ const config = require("@shawnsandy/mix/config/tokens.js");
 module.exports = config({
   source: ["system/_data/tokens/**/*.json"], // tokens dir--/tokens/my-tokens.json
   build: "styles/_tokens/", // build dir trailing slash required--scss/_tokens.scss
-  name: "tokens", // name of you scss token output file--tokens.scss
+  name: "_tokens", // name of you scss token output file--tokens.scss
 });
