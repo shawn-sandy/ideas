e30a73f5d330a340b8a89ffb6569944df95654c7
Shawn Sandy <38263979+shawn-sandy@users.noreply.github.com>
2020-04-20 22:22:49 -0400

    Combine tokens for @ideas/local system

--- tokens.js
+++ tokens.js
@@ -4,8 +4,8 @@ const config = require("@shawnsandy/mix/config/tokens.js");
 
 module.exports = config({
   source: [
-    "system/_data/tokens/**/*.json",
     "node_modules/@shawnsandy/mix/tokens/**.json",
+    "system/_data/tokens/**/*.json",
   ], // tokens dir--/tokens/my-tokens.json
   build: "system/styles/_tokens/", // build dir trailing slash required--scss/_tokens.scss
   name: "_tokens", // name of you scss token output file--tokens.scss
