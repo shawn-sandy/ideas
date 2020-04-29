0a9cded00f3dfdb5e9c67853acce0c9f7d81ab18
Shawn Sandy <38263979+shawn-sandy@users.noreply.github.com>
2020-04-29 00:09:11 -0400

    apps/token - setup base component - update mix prefix config - add color page to the api -

---
+++ apps/tokens/src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div id="app">
+    <color-token></color-token>
+  </div>
+</template>
+
+<script>
+  import ColorToken from './components/color.vue'
+  import Axios from "axios"
+
+  export default {
+    name: 'App',
+    components: {
+      ColorToken
+    }
+  }
+</script>
+
+<style lang="scss">
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
+</style>
