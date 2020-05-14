4c6801d72b0c20e6a48dbc365d39f7b31813381c
Shawn Sandy <38263979+shawn-sandy@users.noreply.github.com>
2020-05-14 07:58:04 -0400

    @mix -- refactor to mobile first approach

--- styles.js
+++ styles.js
@@ -1,5 +1,4 @@
 import "@fortawesome/fontawesome-free/css/regular.css";
-import "@shawnsandy/bootstrap-lite";
 import "@shawnsandy/mix/foundation.scss";
 // import "mvp/mvp.css";
 import "./system/styles/styles.scss";
