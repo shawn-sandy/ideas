e957156c908724064554f41e092c6cba38f92624
Shawn Sandy <38263979+shawn-sandy@users.noreply.github.com>
2020-05-18 19:12:00 -0400

    Update mix build scripts - Scripts parcel butls - update npm start --open

--- apps/mix/builds/index.js
+++ apps/mix/builds/index.js
@@ -3,4 +3,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{}],"Focm":[function(require,module,exports) {
 "use strict";require("normalize.css/normalize.css"),require("../sass/foundation.scss");
 },{"normalize.css/normalize.css":"DOAq","../sass/foundation.scss":"DOAq"}]},{},["Focm"], null)
-//# sourceMappingURL=/src.e31bb0bc.js.map
+//# sourceMappingURL=/index.js.map
