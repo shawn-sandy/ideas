3dac1cbe6326e8ccc40b3a172590f262c617b000
Shawn Sandy <38263979+shawn-sandy@users.noreply.github.com>
2020-07-05 18:50:52 -0400

    Refines the grid flex properties

--- apps/mix/dist/styles.7b0c6d85.js
+++ apps/mix/dist/styles.7b0c6d85.js
@@ -221,7 +221,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "59482" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "54328" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
