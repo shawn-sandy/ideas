b1e3cd9ce18f3d7078ec37d8ddf422e9373a8867
shawn-sandy <creative@shawnsandy.com>
2020-07-19 14:36:21 -0400

    lodash run-s test

--- system/js/_array.js
+++ system/js/_array.js
@@ -1,7 +1,7 @@
 /**
  * @license
  * Lodash (Custom Build) <https://lodash.com/>
- * Build: `lodash include="chunk,size" -o system/js/_array.js`
+ * Build: `lodash include="chunk,size" -o system/js/_array.js -m`
  * Copyright JS Foundation and other contributors <https://js.foundation/>
  * Released under MIT license <https://lodash.com/license>
  * Based on Underscore.js 1.8.3 <http://underscorejs.org/LICENSE>
