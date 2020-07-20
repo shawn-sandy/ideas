882418477e8ca893a6b3427b8cbb99b9f17c6341
shawn-sandy <creative@shawnsandy.com>
2020-07-20 14:43:12 -0400

    Adds ol/ul list components styles

--- apps/mix/dist/src.e31bb0bc.js
+++ apps/mix/dist/src.e31bb0bc.js
@@ -221,7 +221,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "50587" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "51374" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
