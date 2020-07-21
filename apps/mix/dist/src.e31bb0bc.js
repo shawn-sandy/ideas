c3e5701e21bcaa9170429ef5a1179cf74620e62e
shawn-sandy <creative@shawnsandy.com>
2020-07-21 07:46:09 -0400

    Updates images refine styles

--- apps/mix/dist/src.e31bb0bc.js
+++ apps/mix/dist/src.e31bb0bc.js
@@ -221,7 +221,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "51374" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "60867" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
