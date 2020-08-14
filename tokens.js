a78875f5fff4e093b55a6efd0b0281027e116dc9
Shawn Sandy <creative@shawnsandy.design>
2020-08-14 19:51:38 -0400

    update format updates

--- tokens.js
+++ tokens.js
@@ -1,12 +1,12 @@
 // config.js
-const config = require("@shawnsandy/mix/config/tokens.js");
+const config = require('@shawnsandy/mix/config/tokens.js')
 // const config = require("./system/styles/_tokens/_tokens.scss");
 
 module.exports = config({
   source: [
-    "node_modules/@shawnsandy/mix/tokens/**.json",
-    "system/_data/tokens/**/*.json",
+    'node_modules/@shawnsandy/mix/tokens/**.json',
+    'system/_data/tokens/**/*.json'
   ], // tokens dir--/tokens/my-tokens.json
-  build: "system/styles/_tokens/", // build dir trailing slash required--scss/_tokens.scss
-  name: "_tokens", // name of you scss token output file--tokens.scss
-});
+  build: 'system/styles/_tokens/', // build dir trailing slash required--scss/_tokens.scss
+  name: '_tokens' // name of you scss token output file--tokens.scss
+})
