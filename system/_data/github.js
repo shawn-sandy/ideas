7c3632a50652549af36915e47be6f98d769716f5
Shawn Sandy <creative@shawnsandy.design>
2020-08-29 10:16:46 -0400

    Update the package main attribute

--- system/_data/github.js
+++ system/_data/github.js
@@ -1,34 +1,34 @@
-const fetch = require("node-fetch");
-const flatcache = require("flat-cache");
-const path = require("path");
+const fetch = require('node-fetch')
+const flatcache = require('flat-cache')
+const path = require('path')
 
-function getCacheKey() {
-  let date = new Date();
+function getCacheKey () {
+  const date = new Date()
   return `${date.getUTCFullYear()}-${
     date.getUTCMonth() + 1
-  }-${date.getUTCDate()}`;
+  }-${date.getUTCDate()}`
 }
 
 module.exports = async function () {
-  let cache = flatcache.load("gitstatus.json", path.resolve("./system/_data"));
-  let key = getCacheKey();
-  let cachedData = cache.getKey(key);
+  const cache = flatcache.load('gitstatus.json', path.resolve('./system/_data'))
+  const key = getCacheKey()
+  const cachedData = cache.getKey(key)
   if (!cachedData) {
-    console.log("Fetching new github stargazers count…");
+    console.log('Fetching new github stargazers count…')
 
     // GitHub API: https://developer.github.com/v3/repos/#get
-    let newData = await fetch("https://api.github.com/repos/11ty/eleventy")
+    const newData = await fetch('https://api.github.com/repos/11ty/eleventy')
       .then((res) => res.json())
       .then((json) => {
         return {
-          stargazers: json.stargazers_count,
-        };
-      });
+          stargazers: json.stargazers_count
+        }
+      })
 
-    cache.setKey(key, newData);
-    cache.save();
-    return newData;
+    cache.setKey(key, newData)
+    cache.save()
+    return newData
   }
 
-  return cachedData;
-};
+  return cachedData
+}
