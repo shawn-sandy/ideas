02ad68ca9cce41f558e8f7c73b8d0603fb330a86
Shawn Sandy <creative@shawnsandy.design>
2020-09-04 17:32:54 -0400

    updates

---
+++ system/eleventyconfig.js
@@ -0,0 +1,76 @@
+const fs = require('fs')
+
+const htmlmin = require('html-minifier')
+const pluginRss = require('@11ty/eleventy-plugin-rss')
+const markdown = require('@shawnsandy/ideas/lib/markdown')
+
+module.exports = function (eleventyConfig) {
+  eleventyConfig.setBrowserSyncConfig({
+    notify: true,
+    open: true,
+    callbacks: {
+      ready: function (err, bs) {
+        bs.addMiddleware('*', (req, res) => {
+          const content_404 = fs.readFileSync('www/404.html')
+          // Provides the 404 content without redirect.
+          res.write(content_404)
+          // Add 404 http status code in request header.
+          // res.writeHead(404, { "Content-Type": "text/html" });
+          res.writeHead(404)
+          res.end()
+        })
+      }
+    }
+  })
+
+  eleventyConfig.addPassthroughCopy('apps/**/builds/*.css')
+  eleventyConfig.addPassthroughCopy('apps/**/builds/*.js')
+  eleventyConfig.addPassthroughCopy('apps/**/builds/images')
+
+  eleventyConfig.addPassthroughCopy('system/styles/css', 'css')
+  eleventyConfig.addPassthroughCopy('system/js/*.js', 'js')
+  eleventyConfig.addPassthroughCopy('system/images', 'images')
+
+  eleventyConfig.addWatchTarget('www/**/*.css')
+
+  // Minify our HTML
+  eleventyConfig.addTransform('htmlmin', (content, outputPath) => {
+    if (outputPath.endsWith('.html')) {
+      const minified = htmlmin.minify(content, {
+        useShortDoctype: true,
+        removeComments: true,
+        collapseWhitespace: true
+      })
+      return minified
+    }
+    return content
+  })
+
+  /**
+   * Plugin @shawnsandy/ideas
+   *
+   */
+
+  // eleventyConfig.addPlugin(require("@shawnsandy/ideas", {siteMap: ["**/*.njk", "apps/**/*.html"];})); // siteMap defines directory to include
+
+  // eleventyConfig.addPlugin(require("@shawnsandy/ideas"));
+  eleventyConfig.addPlugin(require('./apps/ideas/eleventy'))
+  eleventyConfig.addPlugin(require('./apps/ideas/lib/markdown'))
+
+  eleventyConfig.addPlugin(pluginRss)
+  eleventyConfig.addPlugin(markdown)
+
+  return {
+    dir: {
+      output: './www',
+      input: '.',
+      includes: '/system/_includes',
+      data: '/system/_data'
+    },
+    templateFormats: ['njk', 'html'],
+    htmlTemplateEngine: 'njk',
+    markdownTemplateEngine: 'njk',
+    passthroughFileCopy: true,
+    pathPrefix: '/'
+  }
+}
