c8ada24a9933927c14b05f068d2b1ecb1c47a8b9
Shawn Sandy <creative@shawnsandy.design>
2020-09-05 14:01:38 -0400

    refine the list element style - margin/line-height tweaks

--- projects/first-paint/dist/first-paint.80ee2152.js
+++ projects/first-paint/dist/first-paint.80ee2152.js
@@ -220,7 +220,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "52401" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "57327" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
