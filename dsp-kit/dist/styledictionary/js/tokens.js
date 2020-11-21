20d53235c36f4df31e5b6629ecd4f2d3667ba9e0
Shawn Sandy <creative@shawnsandy.design>
2020-11-21 15:16:01 -0500

    DSP snippets for first-paint HTML markup

--- dsp-kit/dist/styledictionary/js/tokens.js
+++ dsp-kit/dist/styledictionary/js/tokens.js
@@ -1,6 +1,58 @@
 /**
  * Do not edit directly
- * Generated on Mon, 02 Nov 2020 12:28:33 GMT
+ * Generated on Sat, 21 Nov 2020 20:13:17 GMT
  */
 
 export const Dark = "#000000"; // Darl color
+export const TurfGreen = "#1ba41f";
+export const Transparent = "#ffffff";
+export const LightGray = "#e6ecef";
+export const DarkBlue = "#00558c";
+export const LimeGreen = "#ccd814";
+export const SkyBlue = "#019be5";
+export const MidnightBlue = "#1f2e48";
+export const RedPlanet = "#bf0000";
+export const SunsetOrange = "#fdb71a";
+export const TdBrandCarbon = "#1f2024";
+export const SmokeGray = "#53565a";
+export const White = "#ffffff";
+export const DarkGray = "#707070";
+export const Roboto37PtFamily = "Roboto";
+export const Roboto37PtWeight = "Regular";
+export const Roboto14PtFamily = "Roboto";
+export const Roboto14PtWeight = "Regular";
+export const Roboto25PtFamily = "Roboto";
+export const Roboto25PtWeight = "Regular";
+export const Roboto11PtFamily = "Roboto";
+export const Roboto11PtWeight = "Regular";
+export const Roboto28PtFamily = "Roboto";
+export const Roboto28PtWeight = "Regular";
+export const Roboto22PtFamily = "Roboto";
+export const Roboto22PtWeight = "Regular";
+export const Roboto18PtFamily = "Roboto";
+export const Roboto18PtWeight = "Regular";
+export const Roboto16PtFamily = "Roboto";
+export const Roboto16PtWeight = "Regular";
+export const Roboto12PtFamily = "Roboto";
+export const Roboto12PtWeight = "Regular";
+export const Roboto120PtFamily = "Roboto";
+export const Roboto120PtWeight = "Regular";
+export const Roboto50PtFamily = "Roboto";
+export const Roboto50PtWeight = "Regular";
+export const Roboto90PtFamily = "Roboto";
+export const Roboto90PtWeight = "Regular";
+export const Roboto67PtFamily = "Roboto";
+export const Roboto67PtWeight = "Regular";
+export const Roboto37PtSize = "37rem";
+export const Roboto14PtSize = "14rem";
+export const Roboto25PtSize = "25rem";
+export const Roboto11PtSize = "11rem";
+export const Roboto28PtSize = "28rem";
+export const Roboto22PtSize = "22rem";
+export const Roboto18PtSize = "18rem";
+export const Roboto16PtSize = "16rem";
+export const Roboto12PtSize = "12rem";
+export const Roboto120PtSize = "120rem";
+export const Roboto50PtSize = "50rem";
+export const Roboto90PtSize = "90rem";
+export const Roboto67PtSize = "67rem";
