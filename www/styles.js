912fb1f14ac61e017e5fbcfd8dcf24c64055de4c
Shawn Sandy <creative@shawnsandy.design>
2021-01-10 13:18:15 -0500

    Dev build/updates - package scripts update-- clean command

--- www/styles.js
+++ www/styles.js
@@ -228,7 +228,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "59258" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "49947" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
