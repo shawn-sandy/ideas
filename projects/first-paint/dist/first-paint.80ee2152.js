c289b0e15412d756ac5f6bf932b5188317ca9922
Shawn Sandy <creative@shawnsandy.design>
2021-01-20 07:26:31 -0500

    First Paint : Add SASS integration

--- projects/first-paint/dist/first-paint.80ee2152.js
+++ projects/first-paint/dist/first-paint.80ee2152.js
@@ -220,7 +220,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "54014" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "52093" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
