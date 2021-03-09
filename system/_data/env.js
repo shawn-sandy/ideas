a09456e9a18d3978db5e34d13745b0419387563f
Shawn Sandy <creative@shawnsandy.design>
2021-03-09 08:04:10 -0500

    Add .env json data file

---
+++ system/_data/env.js
@@ -0,0 +1,9 @@
+require('dotenv').config()
+
+module.exports = {
+  otherSiteUrl:
+    process.env.OTHER_SITE_URL || 'https://eleventy-env-vars-private.netlify.app',
+  hello: process.env.HELLO || 'Hello not set, but hi, anyway 👋',
+  node_env: process.env.NODE_ENV || null
+
+}
