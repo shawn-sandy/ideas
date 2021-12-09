5cc41204e6f054322e9fabf565953cc74d58bcb5
Shawn Sandy <creative@shawnsandy.design>
2021-12-09 06:55:43 -0500

    feat: commit lint configuration
     - add types config

---
+++ commitlint.types.js
@@ -0,0 +1,18 @@
+// commitlint.types.js, will be picked up by commitlint.config.js
+module.exports = {
+  'type-enum': [
+    2,
+    'always',
+    [
+      'feat',
+      'fix',
+      'docs',
+      'style',
+      'refactor',
+      'test',
+      'ci',
+      'chore',
+      'revert'
+    ]
+  ]
+}
