a70e5bccbd4f71c60ca56e5eee7040ca5f6cf0b2
Shawn Sandy <creative@shawnsandy.design>
2021-12-10 02:31:08 -0500

    build: empty commit test

--- commitlint.config.js
+++ commitlint.config.js
@@ -4,44 +4,7 @@ const Configuration = {
    * Referenced packages must be installed
    */
   extends: ['@commitlint/config-conventional'],
-  /*
-   * Resolve and load conventional-changelog-atom from node_modules.
-   * Referenced packages must be installed
-   */
-  parserPreset: 'conventional-changelog-atom',
-  /*
-   * Resolve and load @commitlint/format from node_modules.
-   * Referenced package must be installed
-   */
-  formatter: '@commitlint/format',
-  /*
-   * Any rules defined here will override rules from @commitlint/config-conventional
-   */
-  rules: {
-    'type-enum': [
-      0,
-      'always',
-      [
-        'feat',
-        'fix',
-        'docs',
-        'style',
-        'refactor',
-        'test',
-        'ci',
-        'chore',
-        'revert'
-      ]
-    ]
-  },
-  /*
-   * Functions that return true if commitlint should ignore the given message.
-   */
-  ignores: [(commit) => commit === ''],
-  /*
-   * Whether commitlint uses the default ignore rules.
-   */
-  defaultIgnores: true,
+
   /*
    * Custom URL to show upon failure
    */
