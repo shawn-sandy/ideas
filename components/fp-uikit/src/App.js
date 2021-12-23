d594f23560b6a6db8d86f6e38ebcf69745a0b973
Shawn Sandy <creative@shawnsandy.design>
2021-12-23 13:18:43 -0500

    ci: add a11y plugin for eslint

--- components/fp-uikit/src/App.js
+++ components/fp-uikit/src/App.js
@@ -1,9 +1,11 @@
 import './App.css'
+import logo from './logo.svg'
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <img src={logo} className="App-logo" />
         <a
           className="App-link"
           href="https://beta.reactjs.org"
