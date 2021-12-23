6790e0b7e51f20c0c7ff38a0a3a4d37bcc553741
Shawn Sandy <creative@shawnsandy.design>
2021-12-23 18:37:29 -0500

    feat(update): add ace-core/react to fp-reactive

--- components/fp-uikit/src/index.js
+++ components/fp-uikit/src/index.js
@@ -7,7 +7,7 @@ import reportWebVitals from './reportWebVitals'
 if (process.env.NODE_ENV !== 'production') {
   const axe = require('@axe-core/react')
   axe(React, ReactDOM, 1000)
-  reportWebVitals(console.log)
+  reportWebVitals()
 }
 
 ReactDOM.render(
@@ -20,4 +20,3 @@ ReactDOM.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals()
