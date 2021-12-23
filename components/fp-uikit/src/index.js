a3127fdc2ea5cba7c8b8ef70c7f9d35e36f5240c
Shawn Sandy <creative@shawnsandy.design>
2021-12-23 18:34:55 -0500

    feat: add ace-core/react to fp-reactive

--- components/fp-uikit/src/index.js
+++ components/fp-uikit/src/index.js
@@ -4,6 +4,12 @@ import './index.css'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
 
+if (process.env.NODE_ENV !== 'production') {
+  const axe = require('@axe-core/react')
+  axe(React, ReactDOM, 1000)
+  reportWebVitals(console.log)
+}
+
 ReactDOM.render(
   <React.StrictMode>
     <App />
