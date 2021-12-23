8c4b92c6ca72998f619d0d7c61ba23c9f7384774
Shawn Sandy <creative@shawnsandy.design>
2021-12-23 13:43:09 -0500

    ci: add a11y plugin for eslint

--- components/fp-uikit/src/App.js
+++ components/fp-uikit/src/App.js
@@ -12,7 +12,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React (Beta)
+          Learn React (Betas)
         </a>
       </header>
     </div>
