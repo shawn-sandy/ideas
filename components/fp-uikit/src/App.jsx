7d65dc4d133f578ab51ae5c0758209e7f3c44909
Shawn Sandy <creative@shawnsandy.design>
2021-12-24 15:21:44 -0500

    chore: change jsx formatter to typescript

--- components/fp-uikit/src/App.jsx
+++ components/fp-uikit/src/App.jsx
@@ -3,14 +3,14 @@ import logo from './logo.svg'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" />
+    <div className='App'>
+      <header className='App-header'>
+        <img src={logo} className='App-logo' />
         <a
-          className="App-link"
-          href="https://beta.reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+          className='App-link'
+          href='https://beta.reactjs.org'
+          target='_blank'
+          rel='noopener noreferrer'
         >
           Learn React...
         </a>
