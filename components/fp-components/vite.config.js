409985014f2c5df7803bb5f9f58d2d35a8e84a9e
Shawn Sandy <creative@shawnsandy.design>
2022-01-19 18:52:36 -0500

    feat: vite build configuration

--- components/fp-components/vite.config.js
+++ components/fp-components/vite.config.js
@@ -1,8 +1,19 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+const path = require('path')
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  lib: {
+    entry: path.resolve(__dirname, './ src / components / index.js'),
+    name: 'App',
+    fileName: (format) => `my-lib.${format}.js`
+  },
+  rollupOptions: {
+    // make sure to externalize deps that shouldn't be bundled
+    // into your library
+    external: ['react', 'react-dom']
+  }
 
 })
