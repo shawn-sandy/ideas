cfe0bd660ee8bc65aa1f8c4361a8c1390cc87183
Shawn Sandy <38263979+shawn-sandy@users.noreply.github.com>
2022-01-29 15:44:13 +0000

    feat: add FirstPaint custom StoryBook theme
     - add theme manager

---
+++ components/fp-reactive/.storybook/FirstPaint.js
@@ -0,0 +1,12 @@
+// .storybook/FirstPaint.js
+
+import { create } from '@storybook/theming'
+
+export default create({
+  base: 'light',
+  brandTitle: 'FirstPaint',
+  brandUrl: 'https://firstpaint.dev',
+  brandImage: 'https://first-paint.netlify.app/img/fp.svg',
+  appBg: 'white',
+  appContentBg: 'white',
+})
