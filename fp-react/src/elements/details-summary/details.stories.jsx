237cabc8fbfc24d1e579872d305498c890da3ebb
Shawn Sandy <38263979+shawn-sandy@users.noreply.github.com>
2022-02-04 09:29:32 +0000

    docs(wip): combine details / summary element into one story

--- fp-react/src/elements/details-summary/details.stories.jsx
+++ fp-react/src/elements/details-summary/details.stories.jsx
@@ -1,31 +1,26 @@
 import React from 'react'
 import Details, { Summary } from './details-summary'
+import { SummaryElement } from './summary.stories'
 // import './index.css'
 
 export default {
   title: 'Elements/Details',
   component: Details,
-  subcomponents: { Summary },
+  subcomponents: { SummaryElement },
   parameters: {
     layout: 'padded'
   }
 }
 
-export const DetailsTempalte = (...args) => (
-  <Details {...args}>
-    <Summary>Summary title</Summary>
-    <p>This is the content for the details summary, a native accordion component</p>
-  </Details>
-)
-
 const Template = (args) => (
   <Details {...args}>
-    <Summary>Template Title</Summary>
-    {args.children}
+    <Summary>{args.title}</Summary>
+    <p>{args.children}</p>
   </Details>)
 
 export const DetailsElement = Template.bind({})
 
 DetailsElement.args = {
-  children: 'This is the content for the details summary, a native accordion component'
+  children: 'Add some content here to make it work',
+  title: "Element Title"
 }
