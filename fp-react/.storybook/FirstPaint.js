05edd77a1e87ebc13a16fa8b17c84ef786ac83fe
Shawn Sandy <creative@shawnsandy.design>
2022-02-13 15:34:35 -0500

    docs: update log url in docs

--- fp-react/.storybook/FirstPaint.js
+++ fp-react/.storybook/FirstPaint.js
@@ -6,7 +6,7 @@ export default create({
   base: 'light',
   brandTitle: 'FirstPaint',
   brandUrl: '/',
-  brandImage: 'https://res.cloudinary.com/dqjs95c7n/image/upload/v1644777569/fp-rainbow-brand_jyqg1a.png',
+  brandImage: 'https://res.cloudinary.com/dqjs95c7n/image/upload/v1644774792/fp-brand-rgb-sm_v1rtjz.svg',
   // UI settings
   appBg: 'white',
   appContentBg: 'white',
