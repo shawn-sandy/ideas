b862cdf2bae182d59f5b6b5891b10497b365db38
Shawn Sandy <creative@shawnsandy.design>
2022-02-19 13:23:39 -0500

    refactor: integrate storybook setting from beta

--- fp-react/.storybook/preview.js
+++ fp-react/.storybook/preview.js
@@ -1,3 +1,8 @@
+import { addDecorator } from '@storybook/react'
+import { withPerformance } from 'storybook-addon-performance'
+
+addDecorator(withPerformance)
+
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
@@ -6,4 +11,26 @@ export const parameters = {
       date: /Date$/,
     },
   },
+  layout: 'centered',
+  html: {
+    highlighter: {
+      showLineNumbers: true, // default: false
+      wrapLines: false, // default: true
+    },
+  },
+  status: {
+    statuses: {
+      released: {
+        background: '#0000ff',
+        color: '#ffffff',
+        description: 'This component is stable and released',
+      },
+      alpha: {
+        background: 'lightgray',
+        color: 'gray',
+        description: 'This component is still in development',
+      }
+    },
+  },
+
 }
