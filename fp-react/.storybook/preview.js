4ab01b1c6432d9496736b3d85971a8c5dc888832
Shawn Sandy <creative@shawnsandy.design>
2022-02-19 12:37:27 -0500

    refactor: rename fp-react to fp-react-beta
     - change fpreact to fp-react create-react typescript

--- fp-react/.storybook/preview.js
+++ fp-react/.storybook/preview.js
@@ -1,8 +1,3 @@
-import { addDecorator } from '@storybook/react'
-import { withPerformance } from 'storybook-addon-performance'
-
-addDecorator(withPerformance)
-
 export const parameters = {
   actions: { argTypesRegex: "^on[A-Z].*" },
   controls: {
@@ -11,26 +6,4 @@ export const parameters = {
       date: /Date$/,
     },
   },
-  layout: 'centered',
-  html: {
-    highlighter: {
-      showLineNumbers: true, // default: false
-      wrapLines: false, // default: true
-    },
-  },
-  status: {
-    statuses: {
-      released: {
-        background: '#0000ff',
-        color: '#ffffff',
-        description: 'This component is stable and released',
-      },
-      alpha: {
-        background: 'lightgray',
-        color: 'gray',
-        description: 'This component is still in development',
-      }
-    },
-  },
-
 }
