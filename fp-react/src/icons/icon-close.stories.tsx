14756898872b76553bf0f3f85c5dda9fcc009323
Shawn Sandy <creative@shawnsandy.design>
2022-02-22 12:00:50 -0500

    docs: Rename CindowClose to Custom icon

--- fp-react/src/icons/icon-close.stories.tsx
+++ fp-react/src/icons/icon-close.stories.tsx
@@ -20,7 +20,7 @@ const ComponentTemplate: ComponentStory<
   typeof WindowClose
 > = (args) => <WindowClose {...args} />
 
-export const WindowCloseIcon = ComponentTemplate.bind({})
+export const CustomIcom = ComponentTemplate.bind({})
 
 // WindowCloseIcon.args = {
 // }
