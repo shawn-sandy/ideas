7bb8c07a30cea335c12be83bae856b754a92188b
Shawn Sandy <creative@shawnsandy.design>
2022-02-21 19:52:34 -0500

    feat(alpha): svg close icon compnent

---
+++ fp-react/src/icons/icon-close.stories.tsx
@@ -0,0 +1,26 @@
+import React from "react"
+import {
+  ComponentMeta,
+  ComponentStory
+} from "@storybook/react"
+import WindowClose from "./icon-close"
+
+export default {
+  title: "Elements/Media/Icons",
+  component: WindowClose,
+  parameters: {
+    layout: "centered",
+    status: {
+      type: "alpha"
+    }
+  }
+} as ComponentMeta<typeof WindowClose>
+
+const ComponentTemplate: ComponentStory<
+  typeof WindowClose
+> = (args) => <WindowClose {...args} />
+
+export const WindowCloseIcon = ComponentTemplate.bind({})
+
+// WindowCloseIcon.args = {
+// }
