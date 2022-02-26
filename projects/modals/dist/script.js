8f0ff0c29fdd04a0fe99b8e780c5fc16a48f2933
Shawn Sandy <creative@shawnsandy.design>
2022-02-26 13:55:49 -0500

    chore: move modals to _project - archived

--- projects/modals/dist/script.js
+++
@@ -1,9 +0,0 @@
-var jsModals = document.querySelectorAll(".js-modal-css");
-var jsModal = Array.prototype.slice.call(jsModals);
-jsModal.forEach(function(button) {
-  // console.log(button);
-  button.addEventListener("click", function(e) {
-    // console.log(e.target.dataset.target);
-    location.hash = e.target.dataset.target;
-  });
-});
