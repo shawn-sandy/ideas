87ef3a47921e434f9a259e8c8e195e59fd96bcb7
Shawn Sandy <creative@shawnsandy.design>
2022-02-28 16:10:38 -0500

    refactor(canary): change mixin to from component-name to module

--- fp-react/src/components/table/fp-table.stories.tsx
+++ fp-react/src/components/table/fp-table.stories.tsx
@@ -12,7 +12,7 @@ import FpTable from "./fp-table"
 export default {
   title: "Components/FpTable",
   component: FpTable,
-  layout: "padded",
+  layout: "centered",
   status: {
     type: "canary"
   }
@@ -21,40 +21,42 @@ export default {
 export const BasicTable: ComponentStory<
   typeof FpTable
 > = () => (
-  <FpTable>
-    <thead>
-      <tr>
-        <th>Name</th>
-        <th>Points</th>
-        <th>Name</th>
-        <th>Points</th>
-      </tr>
-    </thead>
-    <tbody>
-      <tr>
-        <td>Dom</td>
-        <td>6000</td>
-        <td>Dom</td>
-        <td>6000</td>
-      </tr>
-      <tr>
-        <td>Melissa</td>
-        <td>5150</td>
-        <td>Melissa</td>
-        <td>5150</td>
-      </tr>
-      <tr>
-        <td>Melissa</td>
-        <td>5150</td>
-        <td>Melissa</td>
-        <td>5150</td>
-      </tr>
-      <tr>
-        <td>Melissa</td>
-        <td>5150</td>
-        <td>Melissa</td>
-        <td>5150</td>
-      </tr>
-    </tbody>
-  </FpTable>
+  <section style={{ width: "100%", minWidth: "85vw" }}>
+    <FpTable>
+      <thead>
+        <tr>
+          <th>Name</th>
+          <th>Points</th>
+          <th>Name</th>
+          <th>Points</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>Dom</td>
+          <td>6000</td>
+          <td>Dom</td>
+          <td>6000</td>
+        </tr>
+        <tr>
+          <td>Melissa</td>
+          <td>5150</td>
+          <td>Melissa</td>
+          <td>5150</td>
+        </tr>
+        <tr>
+          <td>Melissa</td>
+          <td>5150</td>
+          <td>Melissa</td>
+          <td>5150</td>
+        </tr>
+        <tr>
+          <td>Melissa</td>
+          <td>5150</td>
+          <td>Melissa</td>
+          <td>5150</td>
+        </tr>
+      </tbody>
+    </FpTable>
+  </section>
 )
