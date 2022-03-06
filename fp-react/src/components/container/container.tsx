1d0c9a7fa3de0297c16f0158c8dccc82ffdc9b76
Shawn Sandy <creative@shawnsandy.design>
2022-03-06 12:54:19 -0500

    feat(beta): add container styles

--- fp-react/src/components/container/container.tsx
+++ fp-react/src/components/container/container.tsx
@@ -1,5 +1,4 @@
 import * as React from "react"
-import "./Container.scss"
 
 export interface LayoutProps {
   /**
--- fp-react/src/index.ts
+++ fp-react/src/index.ts
@@ -1,3 +1,5 @@
 import Button from "./elements/buttons/button"
+import Grid from "./components/grid/grid"
+import Container from "./components/container/container"
 
-export { Button }
+export { Button, Grid, Container }
