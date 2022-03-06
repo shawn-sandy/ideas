0910124a0d2f45af05744ec081fce217fc86e4b7
Shawn Sandy <creative@shawnsandy.design>
2022-03-06 14:25:25 -0500

    feat: add FpTable to component exports

--- fp-react/src/index.ts
+++ fp-react/src/index.ts
@@ -1,5 +1,21 @@
 import Button from "./elements/buttons/button"
 import Grid, { GridCell } from "./components/grid/grid"
 import Container from "./components/container/container"
+import FpTable, {
+  FpTableHead,
+  FpTableBody,
+  FpTableRow,
+  FpTableCell
+} from "./components/table/fp-table"
 
-export { Button, Grid, Container, GridCell }
+export {
+  Button,
+  Grid,
+  Container,
+  GridCell,
+  FpTable,
+  FpTableHead,
+  FpTableBody,
+  FpTableRow,
+  FpTableCell
+}
