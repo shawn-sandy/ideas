57e0c4f95ea05ac335610e0cf6c74d35afac2828
Shawn Sandy <creative@shawnsandy.design>
2022-03-07 12:58:47 -0500

    feat: remove figure elements from Img component

--- fp-react/src/elements/imgs/img.tsx
+++ fp-react/src/elements/imgs/img.tsx
@@ -1,5 +1,42 @@
 import * as React from "react"
 
+export interface FpImgProps
+  extends React.ImgHTMLAttributes<HTMLImageElement> {
+  /**
+   * set the object-fit style on the image
+   */
+  fit?: string
+  /**
+   * Image styles object
+   */
+  // styles?: string | React.CSSProperties
+  styles?: any
+  /**
+   * Image class names
+   */
+  classes?: string
+  /**
+   * Handles the image error event for the image
+   */
+  imgError?: () => void
+  /**
+   * Handles the image loaded event for the image
+   */
+  imgLoaded?: () => void
+  /**
+   * Sets the image aspect ratio property
+   */
+  ratio?: string
+  /**
+   * Sets the image placeholder for image errors
+   */
+  imgPlaceholder?: string
+  /**
+   * Aria label for the image
+   */
+  ariaLabel?: string
+}
+
 /**
  * `FpImage` React Image element
  * `FpCaption` : Adds a caption to an image
@@ -64,98 +101,3 @@ const FpImg: React.VFC<FpImgProps> = ({
 }
 
 export default FpImg
-
-export interface FpImgProps
-  extends React.ImgHTMLAttributes<HTMLImageElement> {
-  /**
-   * set the object-fit style on the image
-   */
-  fit?: string
-  /**
-   * Image styles object
-   */
-  // styles?: string | React.CSSProperties
-  styles?: any
-  /**
-   * Image class names
-   */
-  classes?: string
-  /**
-   * Handles the image error event for the image
-   */
-  imgError?: () => void
-  /**
-   * Handles the image loaded event for the image
-   */
-  imgLoaded?: () => void
-  /**
-   * Sets the image aspect ratio property
-   */
-  ratio?: string
-  /**
-   * Sets the image placeholder for image errors
-   */
-  imgPlaceholder?: string
-  /**
-   * Aria label for the image
-   */
-  ariaLabel?: string
-}
-
-type FpFigProps = {
-  children: React.ReactNode
-  classes?: string
-  styles?: any
-  width?: string
-  height?: string
-}
-
-/**
- * Images are displayed in blocks by default
- * The image box wrapper allows you to wrap the image
- * in a the picture and change the display without removing
- * the block display.
- */
-export const FpFig: React.FC<FpFigProps> = ({
-  children,
-  styles,
-  classes,
-  width,
-  height,
-  ...props
-}) => {
-  const defStyles = {
-    "--pic-w": width || "500px"
-  }
-  return (
-    <figure
-      className={classes}
-      style={{ ...defStyles, ...styles }}
-    >
-      {children}
-    </figure>
-  )
-}
-
-// type FpCaptionProps = {
-//   children: React.ReactNode
-//   classes?: string
-//   styles?: any
-// }
-
-export const FpCaption: React.FC<FpFigProps> = ({
-  children,
-  styles,
-  classes,
-  ...props
-}) => {
-  return (
-    <figcaption
-      className={classes}
-      style={styles}
-      {...props}
-    >
-      {children}
-    </figcaption>
-  )
-}
